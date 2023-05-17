4bfae71076cc0eaf06630577383245d36d745987
MubbashirHussain <mubbashirhhussain@gmail.com>
2023-05-17 05:24:00 -0700

    Javascript chapter 1-20

---
+++ app.js
@@ -0,0 +1,681 @@
+
+// ========== Chapter 1 ( Alert ) =============
+// task No 1 ---------
+
+alert("Mubbashir")
+alert("Hussain")
+alert("Mubabshirhhussain@gmail.com")
+alert("03272199402")
+alert("Password Yaad nhi hai 😁")
+
+// task No 2 ---------
+
+// alert"You're learning JavaScript!"
+alert("You're learning JavaScript!")
+
+
+// task No 3 ---------
+
+alert("Dispalying Message By Alert box")
+
+
+
+// ========== Chapter 2 ( Variables for String ) =============
+// task No 1 ---------
+
+var CamelCase ;
+
+// task No 2 ---------
+var My_Variable ;
+My_Variable = " assigning String to Varibales"
+
+// task No 3 ---------
+
+var Team_Unkown = "Team Unkown ";
+alert(Team_Unkown) 
+
+// task No 4 ---------
+
+var bestMan = "Charlie";
+bestMan = " Unkown "
+
+
+// ========== Chapter 3 ( Variables for Numbers ) =============
+// task No 1 ---------
+
+var CaseQty;
+
+// task No 2 ---------
+
+CaseQty = 144;
+
+// task No 3 ---------
+
+var num_1 = "9";
+
+// task No 4 ---------
+
+var Sum_1 ;
+Sum_1 = 2 + 5
+
+// task No 5 ---------
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge; // value = 110
+
+// task No 6 ---------
+
+
+
+var num_2 = 7 ;
+num_2 = num_2 + 3; // value = 10
+
+
+// ========== Chapter 4 ( Variables Name Legal and Illegal ) =============
+// task No 1 ---------
+
+// var product cost = 3.45;
+var productcost = 3.45; // * correct 
+
+// task No 2 ---------
+
+// var Nameofband;
+var NameOfBand; // * correct 
+
+
+// task No 3 ---------
+
+var Legal_varibale = 50;
+
+// task No 4 ---------
+
+var MubabshirHussain ;
+
+// task No 5 ---------
+
+/*
+illegal varibales ⬇️
+
+! var 1512
+! var 188ff
+! var helo world 
+! var %#@@$^&&^^>??! 
+
+legal varibales ⬇️
+
+* var Hello_world
+* var HelloWorld
+* var HELLO152
+* var HELLO155
+*/
+
+// ========== Chapter 5 ( Math Expression 1 ) =============
+// task No 1 ---------
+
+// symbol of  Addition  	    ( + )
+// symbol of  Subtraction  	    ( - )
+// symbol of  Multiplication  	( * )
+// symbol of  Division          ( / )
+
+var num_3 = 14 / 2 //*  value = 7  
+
+// task No 2 ---------
+
+var num = 20 % 6; //* value = 2
+
+
+// task No 3 ---------
+
+// var largeNum = (1000 * 2000)
+
+// task No 4 ---------
+
+var num_4 = num_1 - num_2 //* value = -1
+
+// task No 5 ---------
+ 
+var First_divison = 18
+var Second_divison = 14
+var _Division = First_divison % Second_divison;
+
+// task No 6 ---------
+
+alert( 51 * 214)
+
+// ========== Chapter 6 (Math Expression II) =============
+// task No 1 ---------
+
+var x = x + 1; //* value = 1
+
+// task No 2 ---------
+
+var x = 100;
+x = x--
+
+
+// task No 3 ---------
+
+var x = 50;
+var y = x++; //* value = 51
+
+
+// task No 4 ---------
+
+var y = 50;
+var z = --y; //* value = 49
+
+
+// task No 5 ---------
+
+var num_5 = 50;
+
+// task No 6 ---------
+
+num_5 = num_5 + 1
+var num_6 = num_5
+
+
+// task No 7 ---------
+
+var num_7 = 108;
+num_7++
+alert(num_7)
+
+
+// ========== Chapter 7 (Math Expression III) =============
+// task No 1 ---------
+
+var calculatedNum = 2 + (2 * 6); 
+//* value of Calculated Number is  14
+
+// task No 2 ---------
+
+var calculatedNum = (2 + 2) * 6;
+//* value of Calculated Number is  24
+
+// task No 3 ---------
+
+var calculatedNum = (2 + 2) * (4 + 2);
+//* value of Calculated Number is  24
+
+
+// task No 4 ---------
+
+var calculatedNum = ((2 + 2) * 4) + 2;
+//* value of Calculated Number is  18
+
+// task No 5 ---------
+
+
+var calculatedNum = (2 + 2) * (4 +10);
+//* value of Calculated Number is  56
+
+// task No 6 ---------
+
+var calculatedNum = 2 + 2 * 4 +10;
+//* value of Calculated Number is  20
+
+
+// task No 7 ---------
+
+var calculatedNum = 4 / ( 2 * 4 );
+//* value of Calculated Number is  0.5
+
+// ========== Chapter 8 (Concatenating Text Strings) =============
+// task No 1 ---------
+
+var num_8 = '2'+'2'
+console.log(num_8) //* value = 22
+
+// task No 2 ---------
+
+message = ("Hello," + "Dolly");
+alert(message)
+
+
+// task No 3 ---------
+
+alert("33" + 3);
+// displays in the alert box //* ( 333 )
+
+// task No 4 ---------
+
+alert("Pakistan" + "Zindabad")
+
+// task No 5 ---------
+
+var Str_1 = "hello wolrd" + 5;
+
+// task No 6 ---------
+
+var Str_2 = "hello "
+var Str_3 = "World"
+
+var Str_4 = Str_2 + Str_3 //* value =  hello world
+
+
+
+// ========== Chapter 9 (Prompts) =============
+// task No 1 ---------
+
+var FirstName = prompt("Enter First Name")
+
+// task No 2 ---------
+
+var Country = prompt("Country ? " ,"China")
+
+
+// task No 3 ---------
+
+// var yourName = prompt(Enter Your Name"); //* Wrong 
+var yourName = prompt("Enter Your Name"); //* correct
+
+
+// task No 4 ---------
+
+prompt("What You Learn At this time ?" , "Javascript")
+
+// task No 5 ---------
+
+var Str_5 = "Enter Your Last name"
+var Str_6 = "Hussain"
+var Str_7 = prompt(Str_5 , Str_6) //* value = Hussain
+
+// task No 6 ---------
+
+alert(Str_7)
+
+
+
+// ========== Chapter 10 (if statments) =============
+// task No 1 ---------
+
+var City = "karachi"
+if(City == "karachi"){
+    console.log("The City of Light")
+} 
+
+// task No 2 ---------
+var x = 0
+var y = 0
+if (x === y) {
+    var z = prompt("Enter A value of Z")
+}
+
+// task No 3 ---------
+
+var ZipCode = 10010;
+
+if(ZipCode == 10010){
+    alert("Karachi")
+}else{
+    alert("Please write correct city")
+}
+
+// task No 4 ---------
+
+x = 1;
+if(x == 1){
+    x = 0 ;
+} 
+
+// ========== Chapter 11 (Comparison Operators) =============
+// task No 1 ---------
+
+if(x != y){
+    console.log(" X and are NOT Same")
+}
+// task No 2 ---------
+
+x = 5
+if(x >= y){
+    console.log(" X is Greater Than Y ")
+} 
+
+
+// task No 3 ---------
+
+if(x != 4 ){
+    x = 5;
+}
+
+// task No 4 ---------
+
+if(51 != 49){
+    alert("congratulations")
+}
+
+// task No 5 ---------
+
+var FirstName = prompt("Enter you first name" , "Mubbashir")
+if(FirstName != "Mubbashir"){
+    alert("NO Match")
+}
+
+// ========== Chapter 12 (if…else and else if statements) =============
+// task No 1 ---------
+
+var x = 5
+var y = 5
+if(x  == y ){
+    alert(" X and Y are same")
+}else{
+    
+    alert(" X and Y are NOT same")
+}
+
+// task No 2 ---------
+
+var User_Percentage = 84
+if(User_Percentage >= 80){
+    alert("You got a ( A ) Grade in Exam")
+}else if(User_Percentage >= 60){
+    alert("You got a ( B ) Grade in Exam")
+}else if(User_Percentage >= 50){
+    alert("You got a ( c ) Grade in Exam")
+}
+
+// task No 3 ---------
+var a = 20;
+if (a === 10) {
+    alert("a is 10");
+}else{
+    alert("Value Of a is " + a)
+}
+
+
+// task No 4 ---------
+
+var city_1  = prompt("Enter your city Name")
+
+if(city_1 == "karachi"){
+    alert("acknowledging it is Karachi")
+}else if( city_1 == "lahore"){
+    alert("acknowledging it is lahore")
+    
+}else{
+    alert("this City is not Available In Our list")
+}
+
+
+// ========== Chapter 12 (if…else and else if statements) =============
+// task No 1 ---------
+
+var a = 15
+var b = 15
+var c = 8
+var d = 8
+if(a == b && c == d ){
+    console.log("they Both are same ( true )")
+}
+
+// task No 2 ---------
+
+d  = 14
+if(a == b && c != d ){
+    console.log(" c doesn't have the same value as d")
+}
+
+// task No 3 ---------
+
+var User_name_1 = "Hamza", User_name_2 = "arslan"
+var User_age_1 = 65 , User_age_2 = 52
+if(User_name_1 == "hamza" || User_name_2 == "arslan" && User_age_1 <= 60 && User_age_2 <= 60){
+    console.log("One of Them is ture")
+}
+
+
+// task No 4 ---------
+
+var x = 51 
+var y = 15 
+if(x >= y){
+    alert("X Is greater then Y")
+}else if(y >= x){
+    
+    alert("Y Is greater then X")
+}
+
+// task No 5 ---------
+
+var My_firstName = "Mubbashir"
+var My_LastName = "Hussain"
+var User_firstName = prompt("Enter You First Name" , "Mubbashir")
+var User_lastName = prompt("Enter You Last Name" , "Hussain")
+
+if(My_firstName == User_firstName && My_LastName == User_lastName){
+    alert("You're Match")
+}
+
+
+// ========== Chapter 14 (If statements nested) =============
+// task No 1 ---------
+
+var User_password = prompt("Enter Your Password ","passWord")
+if(User_password != ''){
+    if(User_password.length >= 5){
+        alert("OK")
+    }else if(User_password.length < 5){
+        alert("Please Enter Password Greater Then 5 Char.")
+    }
+}else{
+    alert("You Password Is empty please Enter Password")
+}
+
+// task No 2 ---------
+
+var a = 1 , c = "Max"
+if (a === 1) {
+    if (c === "Max") {
+        alert("OK");
+    }
+}
+    
+
+// task No 3 ---------
+
+if( a === 1 && c === "Max"){
+    alert("OK")
+}
+
+// task No 4 ---------
+
+var num_9 = 31
+var num_10 = 31
+if(num_9 == num_10){
+    if(num_9 <= num_10){
+        alert("IF Else IF statment end")
+    }
+}
+
+// ========== Chapter 15 (Array I) =============
+// task No 1 ---------
+
+var Arr_1 = [];
+
+// task No 2 ---------
+
+Arr_1 = ["Hello World"]
+
+
+// task No 3 ---------
+
+var alphabet = ["h","i","j","k"]
+alert(alphabet[2])
+
+// task No 4 ---------
+
+var alphabet=["h","i","j","k", "l","m", "n", "o"]
+console.log(alphabet.length)
+
+
+// task No 5 ---------
+
+var Arr_2 = ["Hello wolrd"]
+Arr_2[1] = "ADD 2nd element by usign index"
+alert(Arr_2[1])
+
+
+// ========== Chapter 16 (Array II) =============
+// task No 1 ---------
+
+var Arr_3 = ["AKber"]
+Arr_3.push("Haider")
+alert(Arr_3[1])
+// task No 2 ---------
+
+var Alphabet=["h","i","j","k"]
+Alphabet.pop()
+
+// task No 3 ---------
+
+
+var Alphabet=["h","i","j","k"]
+Alphabet.push(84)
+
+
+// ========== Chapter 16 (Array III) =============
+// task No 1 ---------
+
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+sizes.shift()
+
+
+// task No 2 ---------
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+sizes.unshift(5,8,6)
+
+
+// task No 3 ---------
+
+var Arr_4 = ["Hello World"]
+Arr_4.unshift("Hello 'JS' ")
+alert(Arr_4[0])
+
+// task No 4 ---------
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+sizes.splice(2,0,"L")
+
+// task No 5 ---------
+
+
+var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+var RegSizes = sizes.slice(0,3)
+
+// task No 6 ---------
+
+var pets = ["dog", "cat", "ox", "duck", "frog"]
+pets.splice(1,3,"Loin","Cheeta","Rabbit")
+
+// task No 7 ---------
+
+var pets = ["dog", "cat", "ox", "duck", "frog"]
+delete pets[1]
+delete pets[2]
+
+// task No 8 ---------
+
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+pets = pets.slice(3,5)
+
+
+// ========== Chapter 17 - 20 (for Loops) =============
+// task No 1 ---------
+
+for (let i = 0; i <= 10; i++) {
+console.log(i)    
+}
+
+// task No 2 ---------
+
+for (let i = 0; i <= 12 ; i++) {
+console.log(i)    
+}
+
+// task No 3 ---------
+
+for (let i = 0; i <= 4; i++){}
+
+// task No 4 ---------
+
+for (let p = 0; p < 101 ; p++) {
+console.log(p)    
+}
+
+
+// task No 5 ---------
+
+for (let l = 3; l > 0; l--) {
+    console.log(l)
+}
+
+// task No 6 ---------
+
+var Arr_5 = [24,51,64]
+
+// task No 7 ---------
+
+var flag = true
+
+// task No 8 ---------
+
+var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+for (let Pe = 0; Pe <= pets.length; Pe++) {
+ console.log(pets[Pe])   
+}
+
+// task No 9 ---------
+
+for (let i = 0; i <= 10; i++) {
+    if(i == 1){
+        alert( i )
+    }
+}
+
+// task No 10 --------
+
+var UserName = ["amin","Asgar","yaseeb",'naseen','ali','kasim']
+var UserNamePRO = prompt("Emter User Name","amin")
+var flag = false
+for (let i = 0; i <= UserName.length ; i++) {
+if(UserName[i] == UserNamePRO){
+    alert(UserName[i]+" Is Found")
+    flag = true
+    break;
+}    
+}if(!flag){
+    alert("User Not Found")
+}
+
+// task No 11 --------
+
+var list = ["amin","Asgar","yaseeb",'naseen','ali','kasim']
+var userInput = prompt("","yaseen")
+var matchFound = false;
+for (var i = 0; i < list.length; i++) {
+ if (userInput === list[i]) {
+ alert("Match found");
+ matchFound = true;
+ break;
+ }
+
+}if(!matchFound){
+    alert("a match isn't found")
+}
+
+// task No 12 --------
+
+
+var firstArr = ["a", "b", "c", "d", "e", "f"];
+var secondArr = [1, 2, 3, 4, 5, 6];
+for (let i = 0; i <= firstArr.length ; i++) {
+    for (let j = 0; j < secondArr.length ; j++) {
+        console.log(firstArr[i] + secondArr[j])
+        
+    }
+}
+
